204e61ac38d96e646c454287e727eb38d1e73fb9
Brayconn <29030756+Brayconn@users.noreply.github.com>
2018-06-13 19:45:48 -0700

    Cleaned up some code/formatting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2
-import sys, argparse, logging, logging.config, socket, bitstring
+import sys, argparse, logging, logging.config, socket, bitstring#, sense_hat
+#from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
 
 parser = argparse.ArgumentParser()
 parser.add_argument("port", help="Port to listen on", type=int)
@@ -10,7 +11,7 @@ logging.config.fileConfig('logging.ini')
 
 # TODO probably needs to be moved to other file eventually
 HOST = args.host
-if( HOST == None):
+if(HOST == None):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send 
     HOST = s.getsockname()[0]
@@ -27,36 +28,30 @@ while 1:
     connection, address = s.accept()
     logging.info('Incoming connection from %s', address)
     while 1:
-        
         rawdata = connection.recv(BUFFERSIZE)
-        print(rawdata)
+        size = sys.getsizeof(rawdata) 
         logging.info('%s', rawdata)
-        if(sys.getsizeof(rawdata) > 0):
-            data = bitstring.ConstBitStream(bytes=rawdata, length=160)
-            print(data)
-            logging.info('%s', data) 
-
-            messageSize = data.read('intle:16')
-            logging.info("Received Message Size: %d", messageSize)
-
-            messageType = data.read('intle:16')
-            logging.info("Received Message Type: %d", messageType)
-
-            messageTime = data.read('intle:64')
-            logging.info("Received Message Time: %d", messageTime)
+        #TODO implement checking for ending message
+        if(size != 20):
+            continue
         
-    # RA: 
-    #ant_pos = data.bitpos
-    #rightAscension = data.read('hex:32')
-    #data.bitpos = ant_pos
-            rightAscension_uint = data.read('uintle:32')
-            logging.info("Destination Right Ascension: %d", rightAscension_uint)
+        data = bitstring.ConstBitStream(bytes=rawdata, length=size*8)
+        logging.info('%s', data) 
+        
+        messageSize = data.read('intle:16')
+        logging.info("Received Message Size: %d", messageSize)
+
+        messageType = data.read('intle:16')
+        logging.info("Received Message Type: %d", messageType)
+        
+        messageTime = data.read('intle:64')
+        logging.info("Received Message Time: %d", messageTime)
+        
+        rightAscension = data.read('uintle:32')
+        logging.info("Destination Right Ascension: %d", rightAscension)
+
+        declination = data.read('intle:32')
+        logging.info("Destination Declination: %d", declination)
     
-    # DEC:
-    #ant_pos = data.bitpos
-    #declination = data.read('hex:32')
-    #data.bitpos = ant_pos
-            declination_int = data.read('intle:32')
-        logging.info("Destination Declination: %d", declination_int)
-    #remember to send coords back to stellarium 10 times in a row
-    #connection.close()
+        #remember to send coords back to stellarium 10 times in a row
+    connection.close()
